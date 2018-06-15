d7e6ddcd075ee3635ec2ccd4c65f1972264c5c7e
Michael Wilson <michael.wilson@waterstonemortgage.com>
2018-06-15 08:58:25 -0500

    Design Controller
    Design Controller created

---
+++ Portfolio/Controllers/DesignController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class DesignController : Controller
+    {
+        // GET: Design
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BlackjackTable()
+        {
+            return View();
+        }
+    }
+}
