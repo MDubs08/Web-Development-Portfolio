726f025baf56a724285034223b9ef57d1d8af30f
Michael Wilson <michael.wilson@waterstonemortgage.com>
2018-06-15 16:44:15 -0500

    RDDBViewEngine Added
    RDDBViewEngine was added to adapt to new partial folder.
    This has also been added to the Global.asax.cs to function as intended

---
+++ Portfolio/App_Start/RDDBViewEngine.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace Portfolio.App_Start
+{
+    public class RDDBViewEngine : RazorViewEngine
+    {
+        private static readonly string[] NewPartialViewFormats =
+        {
+            "~/Views/{1}/Widgets/{0}.cshtml",
+            "~/Views/Shared/Widgets/{0}.cshtml",
+            "~/Views/{1}/Blackjack/{0}.cshtml",
+            "~/Views/Design/Blackjack/{0}.cshtml"
+        };
+
+        public RDDBViewEngine()
+        {
+            PartialViewLocationFormats = base.PartialViewLocationFormats.Union(NewPartialViewFormats).ToArray();
+        }
+    }
+}
